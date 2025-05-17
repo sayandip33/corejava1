020ffa63d30a2712dc7b341355844e0d70acb49d
sayandip sadhu <sayandipsadhu777@gmail.com>
2025-05-17 08:58:57 +0530

    commit 2nd

--- src/First_01_basic/ClassAndVariables.java
+++ src/First_01_basic/ClassAndVariables.java
@@ -3,6 +3,6 @@ package First_01_basic;
 public class ClassAndVariables {
 	public static void main(String[] args) {
 		System.out.println("Jai Baba Sannyasi......");
-		
+		System.out.println("I did some code changes");
 	}
 }
