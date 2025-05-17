1c53b45f9cf54073fa2f5a710cd68c53babf316d
sayandip sadhu <sayandipsadhu777@gmail.com>
2025-05-17 09:10:17 +0530

    new project

--- src/First_01_basic/ClassAndVariables.java
+++
@@ -1,8 +0,0 @@
-package First_01_basic;
-
-public class ClassAndVariables {
-	public static void main(String[] args) {
-		System.out.println("Jai Baba Sannyasi......");
-		System.out.println("I did some code changes");
-	}
-}
---
+++ src/demopackage2/Classes2.java
@@ -0,0 +1,8 @@
+package demopackage2;
+
+public class Classes2 {
+	public static void main(String[] args) {
+		System.out.println("Starting main method......");
+		System.out.println("its all about new project");
+	}
+}
